fbedb2828c0c615a5973afe8fc1611db93813241
Haroldo Cruz <msom.info@gmail.com>
2021-05-12 16:14:03 -0300

    fix(auth): controller
    validating cpf and email, among others.

--- src/features/auth/controller.ts
+++ src/features/auth/controller.ts
@@ -1,9 +1,11 @@
+import { IToken } from './../../models/IToken';
 import { Model, model } from "mongoose";
 import { Request } from "express";
 import { IUser } from './../../models/User';
 import * as MSG from '../../utils/messages';
 import { IAuth } from "../../authServices";
 import Crypt from "./../../utils/security/cryptograph";
+import { Util } from "../../utils/util";
 
 export interface IAuthCtrl {
     'login': (arg0: Request & IAuth, callback: Function) => void;
@@ -28,10 +30,35 @@ function fnLogIn(User: Model<IUser>) {
     return async (req: Request & IAuth, callback: Function) => {
         console.info("\tUSER_LOGIN", req.body);
 
-        //TODO: verificar se o username da requisição é 'email' ou 'cpf'
+        //verificar se o username da requisição é 'email', 'cpf' ou uma expressão inválida
+        // const cpf = Util.isValidCpf(req.body.username)
+        // const email = Util.isValidEmail(req.body.username)
+        const key: string = (() => {
+            let username = req.body.username.trim();
+            if (Util.isValidCpf(username)) {
+                return "cpf";
+            }
+            else if (Util.isValidEmail(username)) {
+                return "dataAccess.email";
+            }
+            else {
+                return "invalid";
+            }
+        })();
+
+        // se username for uma expressão inválida
+        if (key === "invalid") { callback(MSG.errUsername); return; }
+
         try {
             //buscar usuário no DB pelo 'email' ou 'cpf'
-            const user = await User.findOne({ 'cpf': req.body.username }).select('+dataAccess.passwordHash +dataAccess.groupList +dataAccess.groupDefault');
+            const user = await User.findOne({ [key]: req.body.username })
+            .select('+dataAccess.passwordHash +dataAccess.profileList +dataAccess.profileDefault')
+            .populate("dataAccess.profileList dataAccess.profileDefault");
+
+            if(!user.status){
+                callback(MSG.errUserDeactived);
+                return;
+            }
 
             //verificar autenticidade do usuário
             autentication(req, <IUser>user, callback)
@@ -52,8 +79,8 @@ function fnLogIn(User: Model<IUser>) {
         if (await Crypt.compareHash(req.body.password, user.dataAccess.passwordHash)) {
 
             //verifica qual é o perfil padrão
-            // const group = user.dataAccess.groupList.find(function (group){ user.dataAccess.groupDefault == group });
-            const group = user.dataAccess.groupDefault;
+            // const profile = user.dataAccess.profileList.find(function (profile){ user.dataAccess.profileDefault == profile });
+            const profileLogin = user.dataAccess.profileDefault || user.dataAccess.profileList[0];
 
             //pegar o ip do cliente que fez a conexão
             const ipClient = req.connection.remoteAddress || req.socket.remoteAddress;
@@ -61,43 +88,48 @@ function fnLogIn(User: Model<IUser>) {
             //pegar o momento atual (data)
             const actualDate = new Date();
 
-            //gerar um token baseado em:
+            //formatar um token usando:
             // -momento atual;
             // -id do usuário;
             // -ip do cliente (deverá ser criptografado);
             // -grupo/perfil atual do usuário;
             // -lista dos grupos/perfis a qual o usuário pertence (deverá ser criptografado)
-            const token = await Crypt.generateToken({
-                'date': actualDate,
+            const formattedToken: IToken = {
+                'actualDate': actualDate,
                 'id': user._id,
                 'ipClient': await Crypt.encodeTextAES(ipClient),
-                'group': group,
-                'groupList': (user.dataAccess.groupList) ? await Crypt.encodeTextAES(JSON.stringify(user.dataAccess.groupList)) : null
-            });
+                'profileLogin': profileLogin,
+                'profileList': (user.dataAccess.profileList) ? await Crypt.encodeTextAES(JSON.stringify(user.dataAccess.profileList)) : null
+            }
+
+            //gerar o token
+            const token = await Crypt.generateToken(formattedToken);
 
             //atualizar informações de login
             user.loginInfo.lastDate = user.loginInfo.actualDate;
             user.loginInfo.actualDate = actualDate;
             user.loginInfo.ipClient = ipClient;
-            user.loginInfo.group = group;
+            user.loginInfo.profileLogin = profileLogin;
             user.loginInfo.token = token;
 
-            //guardar as novas informações de login no DB
+            //responder e guardar as novas informações de login no DB
             userLoginInfoUpdate(user, callback);
         } else {
             callback(MSG.errPass);
+            return;
         }
     }
 
     async function userLoginInfoUpdate(user: IUser, callback: Function) {
-        console.log(user) //!APAGAR
         User.updateOne({ '_id': user._id }, { $set: user }, {}, (error) => {
             if (error) {
                 console.log("USER_LOGIN_ERROR: " + error);
                 callback(MSG.errLogin);
+                return;
             }
             else {
                 callback(user);
+                return;
             }
         });
     }
@@ -117,6 +149,7 @@ function fnLogOut(User: Model<IUser>) {
         }
 
         callback(MSG.msgSuccess);
+        return;
     }
 }
 
